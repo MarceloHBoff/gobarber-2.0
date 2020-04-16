5746741b13d0830439a343daddc723d832070be4
MarceloHBoff <marceloboff20@gmail.com>
2020-04-15 22:39:30 -0300

    Initial commit

---
+++ backend/src/routes/appointments.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { getRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointmentsRepository = getRepository(AppointmentsRepository);
+  const appointments = appointmentsRepository.find();
+
+  return response.status(200).json(appointments);
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+  const { provider_id, date } = request.body;
+
+  const parsedDate = parseISO(date);
+
+  const createAppointment = new CreateAppointmentService();
+
+  const appointment = await createAppointment.execute({
+    date: parsedDate,
+    provider_id,
+  });
+
+  return response.status(201).json(appointment);
+});
+
+export default appointmentsRouter;
